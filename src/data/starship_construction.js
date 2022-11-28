af6b48931e619f6760cc9c3cf7c0f7627c731332
antaresshipyard <73565991+antaresshipyard@users.noreply.github.com>
2022-11-28 11:49:56 -0500

    Create starship_construction.js

---
+++ src/data/starship_construction.js
@@ -0,0 +1,10 @@
+module.exports = [{
+	type: "starshipconstruction",
+	id: "Con001",
+	set: ["75013"],
+	name: "Federation Prototype",
+	cost: 1,
+	text: "This card can only be equipped to a Unique ship whose name (ignoring registry number) listed after 'U.S.S.' is the same as the ship class. \n-----------------------------------------\n Add 1 [tech], 1 [weapon], or [crew] to this ship's Upgrade Bar. \n <b>WHEN DEFENDING: MODIFY DEFENSE DICE STEP:</b> You may re-roll 1 [blank] or [battlestations] .",
+	OnePerShip: true,
+	factions: ["federation"]
+}];
