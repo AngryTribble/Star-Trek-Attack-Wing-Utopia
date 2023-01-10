37b48c0e39f13619dee238d7f27b94ac5c8235d8
antaresshipyard <73565991+antaresshipyard@users.noreply.github.com>
2023-01-10 17:12:02 -0500

    Fixed Kos'Karii shields

--- src/data/ships.js
+++ src/data/ships.js
@@ -908,7 +908,7 @@ module.exports = [
 		attack: 4,
 		agility: 1,
 		hull: 3,
-		shields: 2,
+		shields: 3,
 		cost: 16,
 		text: "<b>WHEN ATTACKING A SHIP WITH A HULL OF 4 OR GREATER:</b> Steal a [scan] Token, a [battlestations] Token, or an [evade] Token from beside the defending ship and place it beside this ship.\n<b>WHEN ATTACKING A KLINGON SHIP OR A SHIP WITH A KLINGON CAPTAIN EQUIPPED TO IT:</b> Add 1 [hit]. ",
 		unique: true,
